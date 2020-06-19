f6b3055d34ed0a505c2f51d5741215cce4a33295
caglayan <caglayanserbetci@gmail.com>
2020-06-19 16:05:50 +0300

    denemex

--- src/Api/commentApi.js
+++ src/Api/commentApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:4000";
-//const url = "https://ninjaoders-backend.herokuapp.com";
+// const url = "http://localhost:4000";
+const url = "https://ninjaoders-backend.herokuapp.com";
 
 export const pullComments = (skip, limit, courseId) => {
   console.log("Pull Comments Api");
